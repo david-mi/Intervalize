d12e0e68fe1964d1c14cdf3a2657c6e5ad9b7f9c
David Michel <david.michel2809@gmail.com>
2024-02-01 08:23:33 +0100

    feat: add react navigation dependencies and base architecture

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather, Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+function CreatedIntervals() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function CurrentInterval() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Current Interval"
+          component={CurrentInterval}
+          options={{
+            tabBarLabel: "Current Interval",
+            tabBarIcon: () => <Ionicons name="timer-outline" size={24} color="black" />
+          }}
+        />
+        <Tab.Screen
+          name="Created Intervals"
+          component={CreatedIntervals}
+          options={{
+            tabBarLabel: "Created Intervals",
+            tabBarIcon: () => <Feather name="list" size={24} color="black" />
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}

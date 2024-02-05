6ebe984d7fd99988c5b72b6ef4dcd0bc06011972
David Michel <david.michel2809@gmail.com>
2024-02-05 17:35:16 +0100

    feat: add Timer reusable component

---
+++ src/components/CurrentSession/Timer/Timer.tsx
@@ -0,0 +1,35 @@
+import { View, Text } from "react-native";
+import { Time } from "../../../types";
+import TextWithCustomFont from "../../TextWithCustomFont/TextWithCustomFont";
+import { styles } from "./timer.styles";
+import { ComponentProps } from "react";
+
+type Props = Time<string> & {
+  size: "medium" | "big"
+}
+
+function Timer({ minutes, seconds, size }: Props) {
+  const timerContainerStyles = {
+    ...styles.container,
+    ...styles[size]
+  }
+
+  const textStyles = {
+    ...styles.text,
+    ...styles[size]
+  }
+
+  return (
+    <View style={timerContainerStyles}>
+      <TextWithCustomFont style={textStyles} fontFamily="clockicons">
+        {minutes}
+      </TextWithCustomFont>
+      <Text style={textStyles}>:</Text>
+      <TextWithCustomFont style={textStyles} fontFamily="clockicons">
+        {seconds}
+      </TextWithCustomFont>
+    </View>
+  );
+}
+
+export default Timer;
---
+++ src/components/CurrentSession/Timer/timer.styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+    backgroundColor: "black",
+    justifyContent: "center"
+  },
+  text: {
+    color: "white"
+  },
+  big: {
+    fontSize: 80
+  },
+  medium: {
+    fontSize: 40
+  }
+})

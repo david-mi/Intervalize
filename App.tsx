1f8d9ef07b43cb36e459daa71e9aa6eade295922
David Michel <david.michel2809@gmail.com>
2024-02-05 07:23:28 +0100

    feat: add custom fonts and required package for them

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
+import { View } from "react-native";
 import GlobalContextProvider from "./src/context/GlobalContext";
 import Routes from "./src/routes/routes";
 import 'expo-dev-client';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "clockicons": require("./assets/fonts/clockicons.ttf"),
+    "oswald-light": require("./assets/fonts/Oswald-Light.ttf"),
+    "oswald-regular": require("./assets/fonts/Oswald-Regular.ttf"),
+    "oswald-medium": require("./assets/fonts/Oswald-Medium.ttf"),
+    "oswald-bold": require("./assets/fonts/Oswald-Bold.ttf")
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <GlobalContextProvider>
-      <Routes />
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <Routes />
+      </View>
     </GlobalContextProvider>
   );
 }

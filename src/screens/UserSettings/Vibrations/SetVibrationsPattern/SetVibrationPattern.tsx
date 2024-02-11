8ea3016ee7630cdd5fc16cf7962f1f242cf38e92
David Michel <david.michel2809@gmail.com>
2024-02-11 18:14:44 +0100

    refacto: refacto SetVibrationPattern with SetDefaultVibrationPattern

--- src/screens/UserSettings/Vibrations/SetVibrationsPattern/SetVibrationPattern.tsx
+++ src/screens/UserSettings/Vibrations/SetVibrationsPattern/SetVibrationPattern.tsx
@@ -1,21 +1,15 @@
 import * as React from "react"
 import { View } from "react-native";
 
+import SetDefaultVibrationPattern from "./SetDefaultVibrationPattern/SetDefaultVibrationPattern";
 import { styles } from "./setVibrationsPattern.styles"
 
-import Checkbox from "@/components/Checkbox/Checkbox";
 import { GlobalContext } from "@/context/GlobalContext";
-import { UserSettingsContext } from "@/context/UserSettingsContext";
 import { defaultVibrationsPattern } from "@/data/defaultUserSettings";
 
 function SetVibrationPattern() {
-  const { updateUserSettings } = React.useContext(UserSettingsContext)
   const { userSettings } = React.useContext(GlobalContext)
   const [isDefaultPattern, setIsDefaultPattern] = React.useState(checkIfDefaultPattern(userSettings.vibrationPattern))
-
-  function setDefaultVibrationPattern() {
-    updateUserSettings("vibrationPattern", defaultVibrationsPattern)
-  }
 
   function checkIfDefaultPattern(patternToCompare: number[]) {
     for (let i = 0; i < defaultVibrationsPattern.length; i++) {
@@ -27,16 +21,11 @@ function SetVibrationPattern() {
     return true
   }
 
-  React.useEffect(() => {
-    setIsDefaultPattern(checkIfDefaultPattern(userSettings.vibrationPattern))
-  }, [])
-
   return (
     <View style={styles.container}>
-      <Checkbox
-        text="Pattern par défaut"
-        isChecked={isDefaultPattern}
-        onPress={setDefaultVibrationPattern}
+      <SetDefaultVibrationPattern
+        isDefaultPattern={isDefaultPattern}
+        setIsDefaultPattern={setIsDefaultPattern}
       />
     </View>
   );

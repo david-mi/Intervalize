72646947c9e7ad4cc4af23d826d4b6f6c7555034
David Michel <david.michel2809@gmail.com>
2024-03-12 07:20:49 +0100

    fix: fix appStateRef not updating in useLanguageChanges

--- src/hooks/useLanguageChange.tsx
+++ src/hooks/useLanguageChange.tsx
@@ -1,29 +1,13 @@
 import { getLocales } from "expo-localization";
 import i18n from "i18next";
 import React from "react";
-import { AppState } from "react-native";
 
 export function useLanguageChanges() {
-  const appState = React.useRef(AppState.currentState);
-
   React.useEffect(() => {
-    const subscription = AppState.addEventListener("change", (nextAppState) => {
-      const hasComeForeground = appState.current.match(/inactive|background/) && nextAppState === "active"
-
-      if (!hasComeForeground) return
-
-      const deviceLanguage = getLocales()[0].languageCode || "fr"
-      const hasChangedLanguage = deviceLanguage !== i18n.language
-
-      if (hasChangedLanguage) {
-        i18n.changeLanguage(deviceLanguage)
-      }
-
-      appState.current = nextAppState;
-    });
-
-    return () => {
-      subscription.remove();
-    };
+    const deviceLanguage = getLocales()[0].languageCode || "fr"
+    const hasChangedLanguage = deviceLanguage !== i18n.language
+    if (hasChangedLanguage) {
+      i18n.changeLanguage(deviceLanguage)
+    }
   }, [])
 }
